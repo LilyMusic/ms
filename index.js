407cb13b808d40ea7ecfbed4feb106a38d473f52
Astra <107755845+LilyMusic@users.noreply.github.com>
2023-05-24 22:41:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-f=m=>[[u="Years ",31536e6],[u="Months ",2592e6],[u="Days ",864e5],[u="Hours ",36e5],[u="Minutes ",6e4],[u="Seconds",1e3]].reduce((r,[u,t])=>(v=m/t|0,m%=t,v?r+`${v} ${u}`:r),"").trim();module.exports=f;
+module.exports=m=>[[u="Years ",31536e6],[u="Months ",2592e6],[u="Days ",864e5],[u="Hours ",36e5],[u="Minutes ",6e4],[u="Seconds",1e3]].reduce((r,[u,t])=>(v=m/t|0,m%=t,v?r+`${v} ${u}`:r),"").trim();
